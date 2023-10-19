d1e6304f8c9ad04aa6b161ab69edbf591fb57941
gracieS29 <grace29s13@gmail.com>
2023-10-19 07:52:46 -0400

    updated

---
+++ operadores.js
@@ -0,0 +1,39 @@
+/*el segundo trabajo consiste en un archivo JavaScript que permita ingresar dos datos por teclado, 
+este archivo debe realizar las operacione de adicion, sustracion, multiplicacion, division, 
+division modular y exponente*/
+
+/* MULTIPLICACIÓN
+
+let a=prompt("Ingrese el primer número"); 
+let b=prompt("Ingrese el segundo número");
+let var1= parseInt(a);
+let var2=parseInt(b);
+let resultado= var1 * var2;
+alert("El resultado es: "+resultado); */
+
+/* DIVISIÓN
+let n1=prompt("Ingrese el primer número"); 
+let n2=prompt("Ingrese el segundo número");
+let var1= parseInt(n1);
+let var2=parseInt(n2);
+let resultado= var1 / var2; 
+alert("El resultado es: "+resultado); 
+
+*/
+
+
+/* DIVISIÓN MODULAR
+let y=prompt("Ingrese el primer número"); 
+let x=prompt("Ingrese el segundo número");
+let var1= parseInt(x);
+let var2=parseInt(y);
+let resultado= x % y; 
+alert("El resultado es: "+resultado); */
+
+/* EXPONENCIACIÓN */
+let x=prompt("Introduzca el primer número"); 
+let y=prompt("Introduzca el segundo número");
+let var1= parseInt(x);
+let var2=parseInt(y);
+let resultado= x ** y; 
+alert("El resultado es: "+resultado); 
